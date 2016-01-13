4608d46e82d7b757d10d3bb9461e8c7414b7a81a
kunwar14 <rajkunwar.singh@technofocus.in>
2016-01-13 14:33:09 +0530

    Before Revert

--- Getuser.js
+++ Getuser.js
@@ -95,4 +95,36 @@ app.get('/one/:id',function(req,res,next){
 app.listen(process.env.PORT);
 console.log('listening on 3001');
 
+<<<<<<< HEAD
 var html = '<!DOCTYPE html><html><head><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script></head><body><form>Enter Username:<br><input type="text" name="userid" id="uid"><br> </form><button type="button" id="sub-btn">Submit</button><div id="result"></div><script>$("#sub-btn").click(function(){var id = $("#uid").val();$.get("http://graphapi39.azurewebsites.net/one/"+id, function(result){$("#result").html(JSON.stringify(result));});});</script></body></html>';
+=======
+var html = '
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	</head>
+
+
+	<body>
+
+	<form>
+	Enter Username:<br>
+	  <input type="text" name="userid" id="uid">
+	  <br> 
+	  </form>
+	<button type="button" id="sub-btn">Submit</button>
+	<div id="result"></div>
+
+	<script>
+	$("#sub-btn").click(function(){
+		var id = $("#uid").val();
+		$.get("http://rajgraphapi.azurewebsites.net/one/"+id, function(result){
+			$("#result").html(JSON.stringify(result));
+		});
+	});
+	</script>
+	</body>
+	</html>
+';
+>>>>>>> parent of 89ef721... change2016
